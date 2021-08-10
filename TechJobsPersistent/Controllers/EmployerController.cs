ba0a1a0905efe492e236be4f6972462e71bdd662
bluephoenix5 <mia.a.lord@gmail.com>
2021-08-10 11:09:20 -0700

    Start part 2

--- TechJobsPersistent/Controllers/EmployerController.cs
+++ TechJobsPersistent/Controllers/EmployerController.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TechJobsPersistent.Models;
 using TechJobsPersistent.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace TechJobsPersistent.Controllers
-{
+{ 
     public class EmployerController : Controller
     {
+        private DbContext context;
+
+        public EmployerController(DbContext dbContext)
+        {
+            context = dbContext;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
+            List<Employer> employers = context.Employers.ToList();
             return View();
         }
 
---
+++ TechJobsPersistent/ViewModels/AddEmployerViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddEmployerViewModel : Controller
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Location is required")]
+        public string Location { get; set; }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
